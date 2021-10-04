7390737cbe3e5e4f792022d57be0d2e655828654
osKlonowski <osklonowski@gmail.com>
2021-10-04 15:08:51 +0200

    add logo above download btn

--- lib/components/grid_components/download_open_beta.dart
+++ lib/components/grid_components/download_open_beta.dart
@@ -71,41 +71,55 @@ class OpenBeta02 extends StatelessWidget {
             color: Colors.black,
             padding: gridItemMainPadding,
             child: Center(
-              child: GestureDetector(
-                onTap: _redirectToDownload,
-                child: SizedBox(
-                  width: isMobile(context)
-                      ? constraints.maxWidth / 1.45
-                      : constraints.maxWidth / 2.5,
-                  // height: constraints.maxHeight / 1.618,
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 12.0,
-                      vertical: 10.0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: constraints.maxWidth / 3,
+                    height: constraints.maxHeight / 3,
+                    child: Image.asset(
+                      'assets/logo_white.png',
+                      fit: BoxFit.contain,
                     ),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      boxShadow: <BoxShadow>[
-                        BoxShadow(
-                          color: caribbeanGreenLight,
-                          offset: Offset(4, 4),
-                          spreadRadius: 0,
-                          blurRadius: 0,
+                  ),
+                  GestureDetector(
+                    onTap: _redirectToDownload,
+                    child: SizedBox(
+                      width: isMobile(context)
+                          ? constraints.maxWidth / 1.45
+                          : constraints.maxWidth / 2.5,
+                      // height: constraints.maxHeight / 1.618,
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 12.0,
+                          vertical: 10.0,
                         ),
-                      ],
-                    ),
-                    child: Text(
-                      'Download',
-                      textAlign: TextAlign.center,
-                      style: GoogleFonts.poppins(
-                        fontSize:
-                            getFontSize(MediaQuery.of(context).size.width),
-                        fontWeight: FontWeight.w700,
-                        color: Colors.black,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          boxShadow: <BoxShadow>[
+                            BoxShadow(
+                              color: caribbeanGreenLight,
+                              offset: Offset(4, 4),
+                              spreadRadius: 0,
+                              blurRadius: 0,
+                            ),
+                          ],
+                        ),
+                        child: Text(
+                          'Download',
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.poppins(
+                            fontSize:
+                                getFontSize(MediaQuery.of(context).size.width),
+                            fontWeight: FontWeight.w700,
+                            color: Colors.black,
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
+                ],
               ),
             ),
           );
